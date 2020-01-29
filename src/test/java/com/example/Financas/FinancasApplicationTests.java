903fa4086f52214858bf3f024acfa0e5ff870d1d
Orube Tecnologia <vinnicius60@hotmail.com>
2020-01-29 17:29:11 -0300

    ainda nos primeiros testes

--- src/test/java/com/example/Financas/FinancasApplicationTests.java
+++ src/test/java/com/example/Financas/FinancasApplicationTests.java
@@ -4,25 +4,34 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import com.example.Financas.model.entity.Usuario;
 import com.example.Financas.model.repository.UsuarioRepository;
 
-@SpringBootTest
+
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class FinancasApplicationTests {
 
 	@Autowired
 	UsuarioRepository repository;
 	
+	@Autowired
+	TestEntityManager entityManager;
+
 	@Test
 	 void deveVerificarAExistenciaDeUmEmail() {
 		//cenário
 		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-		repository.save(usuario);
+		entityManager.persist(usuario);
 		
 		//ação/execução
 		boolean result = repository.existsByEmail("usuario@email.com");
@@ -30,9 +39,9 @@ class FinancasApplicationTests {
 		//verificação
 
 		Assertions.assertThat(result).isTrue();
-	
 		
 	}
+	
 	@Test
 	void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {
 		//cenário
@@ -44,6 +53,5 @@ class FinancasApplicationTests {
 		//verificacao
 		Assertions.assertThat(result).isFalse();
 	}
-
 
 }
